e8127987a8ba599aad42f8f92f3e641db1a501bd
CLICC User <cliccuser@HP-111.labs.library.ucla.edu>
2019-11-25 15:25:27 -0800

    Integrated edward delegation

---
+++ Assets/Scripts/EnemyScripts/JumpingPatrol.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class JumpingPatrol : Patrol
+{
+    public float jumpForce = 4000f;
+    public float maxJumpInterval = 5;
+
+    private float forceScalar = 500f;
+    private bool grounded;
+    private Rigidbody2D rigidbody2D;
+    private float sideDetectionRayLength;
+    private Vector2 normal;
+    private float gravityForce = 5f;
+
+    /**
+     * \brief The Awake function starts the jumping loop through the JumpRoutine coroutine.
+     */
+    public void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(JumpRoutine());
+        rigidbody2D.mass = 3f;
+        rigidbody2D.gravityScale = gravityForce;
+    }
+
+    public void Start()
+    {
+        base.Start();
+        sideDetectionRayLength = (h / 2) + (0.05f * h);
+    }
+
+    /**
+     * \brief The FixedUpdate function periodically checks if the object is in contact with any surface, and sets m_Grounded to true
+     * if it is.
+     */
+    public void FixedUpdate()
+    {
+        grounded = false;
+
+        Debug.DrawRay(transform.position, new Vector2(0, -sideDetectionRayLength), Color.red);
+        RaycastHit2D groundInfoBottom = Physics2D.Raycast(transform.position, 
+                                                          new Vector2(0, -1),
+                                                          sideDetectionRayLength, 
+                                                          whatIsGround);
+        RaycastHit2D groundInfoLeft = Physics2D.Raycast(transform.position, 
+                                                        new Vector2(-1, 0),
+                                                        sideDetectionRayLength, 
+                                                        whatIsGround);
+        RaycastHit2D groundInfoRight = Physics2D.Raycast(transform.position, 
+                                                         new Vector2(1, 0),
+                                                         sideDetectionRayLength, 
+                                                         whatIsGround);
+        RaycastHit2D groundInfoTop = Physics2D.Raycast(transform.position, 
+                                                       new Vector2(0, 1),
+                                                       sideDetectionRayLength, 
+                                                       whatIsGround);
+        grounded = groundInfoBottom.collider || groundInfoLeft.collider || groundInfoRight.collider || groundInfoTop.collider;
+        normal = new Vector2(0, 0);
+        if (groundInfoBottom.collider) 
+            normal = new Vector2(0, 1);
+        else if (groundInfoLeft.collider) 
+            normal = new Vector2(1, 0);
+        else if (groundInfoRight.collider) 
+            normal = new Vector2(-1, 0);
+        else if (groundInfoTop.collider)
+            normal = new Vector2(0, -1);
+        Debug.DrawRay(transform.position, normal);
+        if (grounded)
+        {
+            rigidbody2D.gravityScale = 0f;
+            GetComponent<Rigidbody2D>().AddForce(forceScalar * (-1 * GetComponent<Rigidbody2D>().mass * normal));
+        }
+        else
+        {
+            rigidbody2D.gravityScale = gravityForce;
+        }
+    }
+
+    /**
+     * \brief The update function does nothing.
+     */
+    public void Update()
+    {
+    }
+
+    /**
+     * \brief The move function does nothing.
+     */
+    public void Move()
+    {
+        
+    }
+
+    /**
+     * \brief A coroutine which periodically makes the object jump if it is grounded. The object will attempt to jump at an interval from 
+     * 0 to m_MaxJumpInterval seconds. The object will randomly jump either to the left, straight up, or to the right. The object
+     * will not jump if it is not in contact with a surface (i.e. m_Grounded is false).
+     */
+    IEnumerator JumpRoutine()
+    {
+        while (true)
+        {
+            bool jump = true;
+            if (grounded && jump)
+            {
+                grounded = false;
+                Vector3 jumpVector =
+                    new Vector3(
+                        Random.Range(-1, 2) * 0.5f * jumpForce * (normal[0] == 0 ? 1 : 2),
+                        jumpForce * Mathf.Sign(normal[1]) * (normal[0] == 0 ? 1 : 0.5f),
+                        0);
+                Debug.Log("normal: " + normal);
+                Debug.Log("jump vector: " + jumpVector);
+                rigidbody2D.AddForce(jumpVector);
+            }
+            yield return new WaitForSeconds(Random.Range(0, maxJumpInterval));
+        }
+    }
+}
+
---
+++ Assets/Scripts/EnemyScripts/Patrol.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public GameObject loseText;
+    public GameObject cameraObject;
+    public float speed = 5f;
+    public LayerMask whatIsGround;
+    private float groundDetectionXOffset;
+    private float groundDetectionYOffset;
+    private float groundDetectionRayLengthScalar;
+    private Vector2 normal;
+
+    protected bool movingRight = true;
+
+    protected Renderer renderer;
+    protected Vector3 topRightCorner, bottomLeftCorner;
+    protected float w, h;
+
+    public void Start()
+    {
+        cameraObject = GameObject.Find("MainCamera");
+        loseText = GameObject.Find("LoseText");
+        renderer = gameObject.GetComponent<Renderer>();
+        topRightCorner = renderer.bounds.max;
+        bottomLeftCorner = renderer.bounds.min;
+        w = topRightCorner.x - bottomLeftCorner.x;
+        h = topRightCorner.y - bottomLeftCorner.y;
+        groundDetectionXOffset = w / 2 + 0.25f * w;
+        groundDetectionYOffset = h / 2;
+        groundDetectionRayLengthScalar = 0.5f;
+    }
+
+    /**
+     * \brief The update function simply calls the Move function.
+     */
+    public void Update()
+    {
+        topRightCorner = renderer.bounds.max;
+        bottomLeftCorner = renderer.bounds.min;
+        normal = transform.up;
+        Move();
+    }
+
+    /**
+     * \brief Moves the object which the script is attached to. If the object detects that it has reached the end of the
+     *  platform or wall it is currently patrolling, it will turn around and move in the opposite direction.
+     */
+    public void Move()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        Vector3 groundRayStart = new Vector3(transform.position[0] + groundDetectionXOffset * Mathf.Sign(normal[1]),
+                                             transform.position[1] - groundDetectionYOffset * Mathf.Sign(normal[1]),
+                                             transform.position[2]);
+        Vector2 groundRay = new Vector2(normal[0], -1 * normal[1] * groundDetectionRayLengthScalar);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundRayStart, groundRay, groundDetectionRayLengthScalar, whatIsGround);
+        Debug.DrawRay(groundRayStart, groundRay, Color.red);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight)
+            {
+                movingRight = false;
+            }
+            else
+            {
+                movingRight = true;
+            }
+            groundDetectionXOffset = -groundDetectionXOffset;
+            Vector3 newScale = transform.localScale;
+            newScale.x *= -1;
+            transform.localScale = newScale;
+            speed = -speed;
+        }
+    }
+
+    /**
+     * \brief The OnTriggerEnter2D function is triggered whenever an object enters the BoxCollider2D trigger attached to the object.
+     * It currently sets the losing text to be visible and stops the camera object associated withe script from moving.
+     */
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Enemy: OnTriggerEnter2D");
+            loseText.SetActive(true);
+            Destroy(collision.gameObject);
+            cameraObject.GetComponent<CameraMover>().movementSpeed = 0;
+        }
+    }
+}
