0001cae460df4944e2c3901b05d2544a07ae3b85
Edward Lu <edwardzlu98@gmail.com>
2019-12-01 15:01:33 -0800

    Modified aerial enemy script, they can move after spawning now'

--- Assets/Scripts/EnemyScripts/AerialPatrol.cs
+++ Assets/Scripts/EnemyScripts/AerialPatrol.cs
@@ -7,12 +7,12 @@ using UnityEngine;
 /// </summary>
 public class AerialPatrol : Patrol
 {
-
     private bool movingDown = true;
 
-    private float initialY;
-
-    private float dist;
+    private float maxDistance = 5.0f;
+    private float wallDetectionRayLengthScalar = 0.2f;
+    private float wallDetectionYOffset;
+    private Vector3 lastTurnPosition;
 
     /// <summary>
     /// This function is called before the first frame update. It stores the initial position of the enemy
@@ -20,7 +20,7 @@ public class AerialPatrol : Patrol
     void Start()
     {
         base.Start();
-        initialY = transform.position.y;
+        wallDetectionYOffset = h / 2 + (3 * h / 8);
     }
 
     /// <summary>
@@ -29,23 +29,27 @@ public class AerialPatrol : Patrol
     void Update()
     {
         transform.Translate(Vector2.down * speed * Time.deltaTime);
-        dist = initialY - transform.position.y;
-        if (dist > 3)
+
+        Vector3 wallDetectionRayStart = transform.position + new Vector3(0, -1 * wallDetectionYOffset * Mathf.Sign(transform.localScale[1]), 0);
+        Vector2 wallDetectionRay = Vector2.down * wallDetectionRayLengthScalar * Mathf.Sign(transform.localScale[1]);
+        Debug.DrawRay(wallDetectionRayStart, wallDetectionRay);
+        RaycastHit2D wallInfo = Physics2D.Raycast(wallDetectionRayStart, wallDetectionRay, wallDetectionRayLengthScalar, whatIsGround);
+        Debug.Log(wallInfo.collider);
+        if (wallInfo.collider || transform.position[1] - lastTurnPosition[1] > maxDistance)
         {
             if (movingDown)
             {
-                transform.eulerAngles = new Vector3(-180, 0, 0);
                 movingDown = false;
             }
-            
-        }
-        else if (dist < 0)
-        {
-            if (movingDown == false)
+            else
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
                 movingDown = true;
             }
+            Vector3 newScale = transform.localScale;
+            newScale.y *= -1;
+            transform.localScale = newScale;
+            speed = -speed;
+            lastTurnPosition = transform.position;
         }
     }
 }
