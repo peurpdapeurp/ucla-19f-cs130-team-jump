96e50baff5ebab81076641ca5f0c19dd465eb1c6
Katherine Kang <nkang0503@ucla.edu>
2019-12-07 19:46:15 -0800

    Added some more XML comments

--- Assets/Scripts/Speeding/Speeding.cs
+++ Assets/Scripts/Speeding/Speeding.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// <para> Script that increases the player speed after encounter the coin for a certain duration </para>
+/// </summary>
+
 public class Speeding : MonoBehaviour
 {
     public float speedBonus;
@@ -14,6 +18,9 @@ public class Speeding : MonoBehaviour
     }
 
     // Update is called once per frame
+    /// <summary>
+    /// <para> Funtion that removes speed upgrade when time is up </para>
+    /// </summary>
     void Update()
     {
         if (speedBonusDuration == 0)
@@ -22,6 +29,9 @@ public class Speeding : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// <para> Funtion that decrease the time left when called </para>
+    /// </summary>
     public void DecreaseDuration()
     {
         if (speedBonusDuration > 0)
@@ -30,6 +40,11 @@ public class Speeding : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// <para> Funtion that add speed when encounter a coin </para>
+    /// </summary>
+    /// <param name="bonus"> the bonus speed </param>
+    /// <param name="duration"> the bonus duration </param>
     public void GetSpeedUpgrade(float bonus, int duration)
     {
         // Debug.Log("duration is: " + duration);
@@ -39,6 +54,9 @@ public class Speeding : MonoBehaviour
         GetComponent<PlayerMovement>().runSpeed += bonus;
     }
 
+    /// <summary>
+    /// <para> Funtion that remove speed upgrade when time is up </para>
+    /// </summary>
     public void RemoveSpeedUpgrade()
     {
         GetComponent<PlayerMovement>().runSpeed -= speedBonus;
